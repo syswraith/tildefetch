9de4955a77a95193afe3d9fdb075fbfe2e79c440
Syswraith <77088155+syswraith@users.noreply.github.com>
2024-05-31 18:37:22 +0530

    Update tildefetch.py

--- tildefetch.py
+++ tildefetch.py
@@ -1,9 +1,19 @@
 import os
-import curses
+import time
 import subprocess
-from rich.console import Console
-from rich.table import Table
-from rich import box
+import curses
+from curses import wrapper
+
+club = """
+
+ ███╗  ██████╗
+██╔██╗██╔════╝
+╚═╝╚═╝██║     
+      ██║     
+      ╚██████╗
+       ╚═════╝
+
+""".splitlines()
 
 sysinfo = {}
 sysinfo['Hostname']=os.uname()[1] # Hostname
@@ -17,11 +27,29 @@ sysinfo['Active users']=str(len(list(set(subprocess.check_output("users").decode
 sysinfo['Uptime']=subprocess.check_output(["uptime", '-s']).decode('utf-8').strip() # Get uptime 
 sysinfo['Shell']=os.environ['SHELL'] # Get shell
 
+stdscr = curses.initscr()
 
-table = Table(title="system info", box=box.MINIMAL_DOUBLE_HEAD)
-table.add_column('Property')
-table.add_column('Value')
-for x in range(len(sysinfo)):
-    table.add_row(list(sysinfo.keys())[x], sysinfo[list(sysinfo.keys())[x]])
-    console = Console()
-console.print(table)
+# Window calculations
+window_y, window_x = curses.LINES, curses.COLS
+window_y = (window_y-1)//2 if window_y%2!=0 else window_y//2
+window_x = (window_x-1)//2 if window_x%2!=0 else window_x//2
+
+text_window = curses.newwin(window_y, window_x, window_y//2, window_x+1)
+ascii_window = curses.newwin(window_y, window_x, 0, 0)
+
+def main(stdscr):
+    curses.curs_set(0)
+    
+    for info in range(len(sysinfo.keys())):
+        key = list(sysinfo.keys())[info]
+        text_window.addstr(info, 0, f'{key}: {sysinfo[key]}')
+    text_window.refresh()
+    
+    for line in range(len(club)):
+        ascii_window.addstr(line+(window_y-len(club)), 2, club[line].center(50))
+    ascii_window.refresh()
+    
+    stdscr.getkey()
+
+curses.endwin() 
+wrapper(main)
